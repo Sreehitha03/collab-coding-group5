2dbd600d32283c8e9d8ae88ad5a019fe7084e196
karan-kant <22bcs051@iiitdwd.ac.in>
2025-09-05 19:28:50 +0530

    GCD of two numbers

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from functions.factorial import factorial
 from functions.prime import is_prime
+from functions.gcd import gcd
 def main():
     print("Welcome to Collab Project - Math Utilities\n")
 
     print("Factorial of 5:", factorial(5))
     print("Is 7 prime?:", is_prime(7))
+    print("GCD of 10 and 15 is:", gcd(10,15))
+
 
 if __name__ == "__main__":
     main()
